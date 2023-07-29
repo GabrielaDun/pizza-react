d42d96772296ea69a2bf2f2675567789a86b751f
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-07-29 12:34:55 +0200

    Finish styling

--- pizza-react/src/component/SingleTable/SingleTable.js
+++ pizza-react/src/component/SingleTable/SingleTable.js
@@ -1,3 +1,5 @@
+import styles from './SingleTable.module.scss'
+
 const SingleTables = () => {
     return (
         <div>
@@ -10,26 +12,35 @@ const SingleTables = () => {
                     <input class="form-control" />
                 </div>
             </div>
-            <div class="row g-3 align-items-center mt-2">
-                <div class="col-1">
-                    <label for="inputPassword6" class="col-form-label">People:</label>
+            <div class={styles.people}>
+                <div class={styles.booked}>
+                <div class={styles.sign}>
+                        <label class="row-form-label">People</label>
+                    </div> 
+                    <div class={styles.form}>
+                        <input class=" form-control " />
+                    </div>
                 </div>
-                <div class="col-1">
-                    <input class=" form-control " />
-                </div>
-                <div class="col-1 ">
-                    /
-                </div>
-                <div class="col-1 ">
-                    <input class="form-control " />
+                <div class={styles.avaliable}>
+                    <div class={styles.sign}>
+                        <label class="row-form-label">/</label>
+                    </div> 
+                    <div class={styles.form}>
+                        <input class=" form-control " />
+                    </div>
                 </div>
             </div>
-            <div class="row g-3 align-items-center mt-2">
-                <div class="col-auto mr-2">
-                    <label for="inputPassword6" class="col-form-label ">Bill:</label>
-                </div>
-                <div class="col-1">
-                    <input class="form-control" />
+            <div class={styles.people}>
+                <div class={styles.booked}>
+                    <div class={styles.sign}>
+                        <label class="row-form-label">Bill:</label>
+                    </div> 
+                    <div class={styles.signTwo}>
+                        <label class="row-form-label">$</label>
+                    </div> 
+                    <div class={styles.form}>
+                        <input class=" form-control " />
+                    </div>
                 </div>
             </div>
             <div className="col mt-4">
