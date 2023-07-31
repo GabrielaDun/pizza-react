3b547b7ed1a16bd36d51470f76a09b7e8f989d6a
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-07-31 16:59:49 +0200

    Added Tables to AllTable component

--- pizza-react/src/component/AllTables/AllTables.js
+++ pizza-react/src/component/AllTables/AllTables.js
@@ -7,26 +7,28 @@ const AllTables = () => {
     const tableData = useSelector(getTableList);
     console.log(tableData);   
     
-
     return (
         <div>
-        <h1 className="mt-5 mb-2">All tables</h1>
+        <h1 className="mt-5 mb-4">All tables</h1>
         <div className="table">
+        {tableData.map(table => (
             <div className={styles.table}>
-                <div className={styles.tab}>
-                    <div className={styles.left}>
-                        <h3 className={styles.number} >Table 1</h3>
-                        <p className={styles.status}><b>Status:</b> Reserved</p>
-                    </div>
-                    <div className={styles.right}>
-                        <button className="btn btn-primary" type="submit">Show more</button>
-                    </div>
+            <div className={styles.tab}>
+                <div className={styles.left}>
+                    <h3 className={styles.number}>{table.name}</h3>
+                    <p className={styles.status}><b>Status: </b>{table.status}</p>
+                </div>
+                <div className={styles.right}>
+                    <button className="btn btn-primary" type="submit">Show more</button>
                 </div>
             </div>
+        </div>
+        ))}
         </div> 
       </div>
   
         );
+    
 };
 
 export default AllTables;
