ce87e28aa53718dab00fdf00dcc663a83cf1cf29
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-08-01 18:28:35 +0200

    Added error message to people form

--- pizza-react/src/component/SingleTable/SingleTable.js
+++ pizza-react/src/component/SingleTable/SingleTable.js
@@ -5,44 +5,62 @@ import { useSelector } from 'react-redux';
 import { getTableById } from '../../redux/tableRedux';
 import { getStatusList } from '../../redux/tableStatusReducer';
 import { Form} from "react-bootstrap";
+import { useState } from 'react';
+import { useForm } from "react-hook-form";
 
 const SingleTables = () => {
-    const {tablesId} = useParams();
-    console.log(tablesId)
 
+    const { register, handleSubmit: validate, formState: {errors}} = useForm();
+
+    const {tablesId} = useParams();
+    console.log(tablesId);
     const table = useSelector(state => getTableById(state, tablesId))
     console.log(table);
-
     const statusData = useSelector(getStatusList);
-    console.log(statusData);
 
+    const [currentStatus, setCurrentStatus] = useState((table && table.status) || ''); 
+    const [seatsTaken, setSeatsTaken] = useState((table && table.seatsTaken) || '');
+    const [seatsAvaliable, setSeatsAvaliable] = useState((table && table.seatsAvaliable) || '');
+    const [bill, setBill] = useState((table && table.bill) || '');
 
-    return (
-        <div>
+    const handleSubmit = () => {
+        if (currentStatus === "Free") {
+            setBill(0);
+        }
+    }
+
+    if(!table) {
+        return <div>Loading...</div>
+    } else {
+        return (
+        <form onSubmit={validate(handleSubmit)}>
             <h1 className="mt-5 mb-2">{table.name}</h1>
-            <form className="row g-3 align-items-center mt-2">
-                <Form.Group>
-                    <Form.Label className="col-auto">
-                        <label className="col-form-label">Status:</label>
-                    </Form.Label>
-                    <Form.Select className="col-auto">
-                        <option>{table.status}</option>
-                        {statusData.map ( status => {
-                            if(status!== table.status) {
-                                return (<option >{status}</option>)
-                            }
-                        })}
-                    </Form.Select>
-                </Form.Group>
-            </form>
+            <Form.Group className="row g-3 align-items-center mt-2">
+                <Form.Label className="col-auto">
+                    <label className="col-form-label">Status:</label>
+                </Form.Label>
+                <Form.Select 
+                    className="col-auto"
+                    onChange={e => setCurrentStatus(e.target.value)}
+                >
+                    <option>{currentStatus}</option>
+                    {statusData.map ( dataStatus => {
+                        if(dataStatus!== table.status) {
+                            return (<option >{dataStatus}</option>)
+                        }
+                    })}
+                </Form.Select>
+            </Form.Group>
             <Form.Group className={styles.people}>
                 <div className={styles.booked}>
                     <Form.Label className={styles.sign}>
                         <label className="row-form-label">People</label>
                     </Form.Label> 
                     <Form.Control 
+                        {...register('seats', {required: true, min: 0, max: 10})}
                         className={styles.form}
-                        value={table.seatsTaken}
+                        value={seatsTaken}
+                        onChange={e => setSeatsTaken(e.target.value)}
                     />
                 </div>
                 <div className={styles.avaliable}>
@@ -50,13 +68,15 @@ const SingleTables = () => {
                         <label className="row-form-label">/</label>
                     </div> 
                     <Form.Control 
+                        {...register('seats', {required: true, min: 0, max: 10})}
                         className={styles.form}
-                        value={table.seatsAvaliable}
+                        value={seatsAvaliable}
+                        onChange={e => setSeatsAvaliable(e.target.value)}
                     />
                 </div>
             </Form.Group>
+            {errors.seats&&<small className={styles.error}>These fields need to numbered between 0 and 10.</small>}
             {table.status==="Busy" && <Form.Group>
-
                     <div className={styles.people}>
                         <div className={styles.booked}>
                             <div className={styles.sign}>
@@ -67,17 +87,19 @@ const SingleTables = () => {
                             </div> 
                             <Form.Control 
                                 className={styles.form}
-                                value={table.bill}
+                                value={bill}
+                                onChange={e => setBill(e.target.value)}
                             />
                         </div>
                     </div>
             </Form.Group>}
             <div className="col mt-4">
-                <button className="col btn btn-primary" type="submit">Show more</button>
+                <button className="col btn btn-primary" type="submit">Update</button>
             </div>
-      </div>
+      </form>
   
         );
+    }
 };
 
 export default SingleTables;
